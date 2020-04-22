fb978a969e7ab13777f71a28649d973a3f50d995
Devout <51583345+devout-mzc@users.noreply.github.com>
2020-04-22 21:08:32 +0800

    1

--- Huain/Controllers/HomeController.cs
+++ Huain/Controllers/HomeController.cs
@@ -1,43 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Huain.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Huain.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public  IActionResult Login(LoginViewModel model,string returnUrl=null)
         {
-            return View();
+            if (ModelState.IsValid)
+            { 
+            
+            }
+            return View(model);
         }
-        //登录
-        public ActionResult Login()
-        {
-            return View();   
-         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
     }
 }
---
+++ Huain/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Huain.Models
+{
+    public class LoginViewModel
+    {
+        //用户账号-可以是电话号、Email 
+        [Required]
+        public string UserCode { get; set; }
+        //密码
+        [Required]
+        [DataType(DataType.Password)]
+        public string PoassWord { get; set; }
+        [Display(Name ="Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
