beb884c4c3186c6fbb135effc1dd0aafd2eed3f5
Devout <51583345+devout-mzc@users.noreply.github.com>
2020-04-22 20:17:06 +0800

    1

--- Huain/Controllers/HomeController.cs
+++ Huain/Controllers/HomeController.cs
@@ -27,11 +27,17 @@ namespace Huain.Controllers
         {
             return View();
         }
+        //登录
+        public ActionResult Login()
+        {
+            return View();   
+         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
